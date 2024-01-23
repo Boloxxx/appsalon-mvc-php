f46fdb6f45a60e590f93448c8c29676947ca15b6
Boloxxx <bryanrodriguezalmonte@gmail.com>
2024-01-23 16:09:16 -0400

    subir

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -8,7 +8,7 @@
         <div class="barra-servicios">
             <a href="/admin" class="boton">Ver Citas</a>
             <a href="/servicios" class="boton">Ver Servicios</a>
-            <a href="/servicios-crear" class="boton">Nuevo Servicio</a>
+            <a href="/servicios/crear" class="boton">Nuevo Servicio</a>
         </div>
 
 
